3b51dc20826179eb1fe1802bd57c9a60b80f2a9b
MEZIANE <nour.meziane@mediaschool.me>
2024-01-22 22:08:30 +0100

    bdd

--- Bdd.sql
+++ Bdd.sql
@@ -9,7 +9,7 @@ CREATE TABLE `user` (
   `email` varchar(255) DEFAULT NULL,
   `telephone` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`iduser`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) 
 
 -- Contract table structure
 CREATE TABLE `contrat` (
@@ -19,7 +19,7 @@ CREATE TABLE `contrat` (
   `etatCon` varchar(255) DEFAULT NULL,
   `description` text,
   PRIMARY KEY (`numC`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) 
 
 -- Owner table structure
 CREATE TABLE `proprietaire` (
@@ -27,7 +27,7 @@ CREATE TABLE `proprietaire` (
   `dateDebCollab` date DEFAULT NULL,
   `dateFinCollab` date DEFAULT NULL,
   PRIMARY KEY (`idproprietaire`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) 
 
 -- Activities table structure
 CREATE TABLE `activites` (
@@ -35,7 +35,7 @@ CREATE TABLE `activites` (
   `nomAC` varchar(255) DEFAULT NULL,
   `prixAC` decimal(10,2) DEFAULT NULL,
   PRIMARY KEY (`codeAC`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) 
 
 -- Stations table structure
 CREATE TABLE `stations` (
@@ -45,15 +45,13 @@ CREATE TABLE `stations` (
   `CP` varchar(255) DEFAULT NULL,
   `ville` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`codeSta`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) 
 
 -- Region table structure
 CREATE TABLE `region` (
   `codeREG` int NOT NULL AUTO_INCREMENT,
   `nomReg` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`codeREG`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+)
 
--- Dwellings table structure
-CREATE TABLE `habitations` (
-  `refH
+
