d1845548b97880a3b047989e265394c3348db291
Yamcha95 <beghdadimehdi95@gmail.com>
2024-01-23 20:53:36 +0100

    Modif

--- Bdd.sql
+++ Bdd.sql
@@ -1,13 +1,13 @@
 -- User table structure
 CREATE TABLE `user` (
   `iduser` int NOT NULL AUTO_INCREMENT,
-  `nom` varchar(255) DEFAULT NULL,
-  `prenom` varchar(255) DEFAULT NULL,
-  `adresse` varchar(255) DEFAULT NULL,
-  `CP` varchar(255) DEFAULT NULL,
-  `ville` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `telephone` varchar(255) DEFAULT NULL,
+  `nom` varchar(35) NOT NULL,
+  `prenom` varchar(35) NOT NULL,
+  `adresse` varchar(255) NOT NULL,
+  `CP` int NOT NULL,
+  `ville` varchar(35) NOT NULL,
+  `email` varchar(255),
+  `telephone` int NOT NULL,
   PRIMARY KEY (`iduser`)
 ) 
 
@@ -16,7 +16,7 @@ CREATE TABLE `contrat` (
   `numC` int NOT NULL AUTO_INCREMENT,
   `dateCon` date DEFAULT NULL,
   `dateFin` date DEFAULT NULL,
-  `etatCon` varchar(255) DEFAULT NULL,
+  `etatCon` varchar(255) NOT NULL,
   `description` text,
   PRIMARY KEY (`numC`)
 ) 
@@ -32,7 +32,7 @@ CREATE TABLE `proprietaire` (
 -- Activities table structure
 CREATE TABLE `activites` (
   `codeAC` int NOT NULL AUTO_INCREMENT,
-  `nomAC` varchar(255) DEFAULT NULL,
+  `nomAC` varchar(255),
   `prixAC` decimal(10,2) DEFAULT NULL,
   PRIMARY KEY (`codeAC`)
 ) 
@@ -40,17 +40,17 @@ CREATE TABLE `activites` (
 -- Stations table structure
 CREATE TABLE `stations` (
   `codeSta` int NOT NULL AUTO_INCREMENT,
-  `nomSta` varchar(255) DEFAULT NULL,
-  `adresse` varchar(255) DEFAULT NULL,
-  `CP` varchar(255) DEFAULT NULL,
-  `ville` varchar(255) DEFAULT NULL,
+  `nomSta` varchar(255) NOT NULL,
+  `adresse` varchar(255) NOT NULL,
+  `CP` int NOT NULL,
+  `ville` varchar(255) NOT NULL,
   PRIMARY KEY (`codeSta`)
 ) 
 
 -- Region table structure
 CREATE TABLE `region` (
   `codeREG` int NOT NULL AUTO_INCREMENT,
-  `nomReg` varchar(255) DEFAULT NULL,
+  `nomReg` varchar(255) NOT NULL,
   PRIMARY KEY (`codeREG`)
 )
 
