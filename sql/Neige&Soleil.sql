65e49c3f34e91ea4aedb2098594dba1afe0e0e57
MEZIANE <nour.meziane@mediaschool.me>
2024-01-30 19:51:59 +0100

    bon changement

--- sql/Neige&Soleil.sql
+++ sql/Neige&Soleil.sql
@@ -2,59 +2,59 @@
 CREATE DATABASE Neigeandsoleil
 
 -- User table structure
-CREATE TABLE `user` (
-  `iduser` int NOT NULL AUTO_INCREMENT,
-  `nom` varchar(35) NOT NULL,
-  `prenom` varchar(35) NOT NULL,
-  `adresse` varchar(255) NOT NULL,
-  `CP` int NOT NULL,
-  `ville` varchar(35) NOT NULL,
-  `email` varchar(255),
-  `telephone` int NOT NULL,
-  PRIMARY KEY (`iduser`)
+CREATE TABLE user (
+  iduser int NOT NULL AUTO_INCREMENT,
+  nom varchar(35) NOT NULL,
+  prenom varchar(35) NOT NULL,
+  adresse varchar(255) NOT NULL,
+  CP int NOT NULL,
+  ville varchar(35) NOT NULL,
+  email varchar(255),
+  telephone int NOT NULL,
+  PRIMARY KEY (iduser)
 );
 
 -- Contract table structure
-CREATE TABLE `contrat` (
-  `numC` int NOT NULL AUTO_INCREMENT,
-  `dateCon` date DEFAULT NULL,
-  `dateFin` date DEFAULT NULL,
-  `etatCon` varchar(255) NOT NULL,
-  `description` text,
-  PRIMARY KEY (`numC`)
+CREATE TABLE contrat (
+  numC int NOT NULL AUTO_INCREMENT,
+  dateCon date DEFAULT NULL,
+  dateFin date DEFAULT NULL,
+  etatCon varchar(255) NOT NULL,
+  description text,
+  PRIMARY KEY (numC)
 );
 
 -- Owner table structure
-CREATE TABLE `proprietaire` (
-  `idproprietaire` int NOT NULL AUTO_INCREMENT,
-  `dateDebCollab` date DEFAULT NULL,
-  `dateFinCollab` date DEFAULT NULL,
-  PRIMARY KEY (`idproprietaire`)
+CREATE TABLE proprietaire (
+  idproprietaire int NOT NULL AUTO_INCREMENT,
+  dateDebCollab date DEFAULT NULL,
+  dateFinCollab date DEFAULT NULL,
+  PRIMARY KEY (idproprietaire)
 );
 
 -- Activities table structure
-CREATE TABLE `activites` (
-  `codeAC` int NOT NULL AUTO_INCREMENT,
-  `nomAC` varchar(255),
-  `prixAC` decimal DEFAULT NULL,
-  PRIMARY KEY (`codeAC`)
+CREATE TABLE activites (
+  codeAC int NOT NULL AUTO_INCREMENT,
+  nomAC varchar(255),
+  prixAC decimal DEFAULT NULL,
+  PRIMARY KEY (codeAC)
 );
 
 -- Stations table structure
-CREATE TABLE `stations` (
-  `codeSta` int NOT NULL AUTO_INCREMENT,
-  `nomSta` varchar(255) NOT NULL,
-  `adresse` varchar(255) NOT NULL,
-  `CP` int NOT NULL,
-  `ville` varchar(255) NOT NULL,
-  PRIMARY KEY (`codeSta`)
+CREATE TABLE stations (
+  codeSta int NOT NULL AUTO_INCREMENT,
+  nomSta varchar(255) NOT NULL,
+  adresse varchar(255) NOT NULL,
+  CP int NOT NULL,
+  ville varchar(255) NOT NULL,
+  PRIMARY KEY (codeSta)
 );
 
 -- Region table structure
-CREATE TABLE `region` (
-  `codeREG` int NOT NULL AUTO_INCREMENT,
-  `nomReg` varchar(255) NOT NULL,
-  PRIMARY KEY (`codeREG`)
+CREATE TABLE region (
+  codeREG int NOT NULL AUTO_INCREMENT,
+  nomReg varchar(255) NOT NULL,
+  PRIMARY KEY (codeREG)
 );
 
 
--- sql/data.sql
+++ sql/data.sql
@@ -1,26 +1,38 @@
--- Insertion dans la table Constructeur
-INSERT INTO Constructeur (idconstructeur, nom, pays, statut)
-VALUES (1, 'Boeing', 'États-Unis', 'Actif'),
-       (2, 'Airbus', 'France', 'Actif');
+-- Insertion dans la table Utilisateur
+INSERT INTO Utilisateur (iduser, nom, prenom, adresse, CP, ville, email, telephone )
+VALUES (1, 'mehdi', 'beghdadi', '8 impasse des 2 couzin','75017', 'periere levallois','mehdibeghdadi@gmail.com' '06.65.62.45.65') 
+       (2, 'nour', 'meziane', '10 rue de la maze bank','75010', 'boullogne','nourmeziane@gmail.com' '06.25.85.75.65');
 
--- Insertion dans la table Avion
-INSERT INTO User (iduser, designation, capacite, dateAchat, idconstructeur)
-VALUES (1, 'Boeing 737', 150, '2022-05-10', 1),
-       (2, 'Airbus A320', 180, '2022-06-15', 2),
-       (3, 'Boeing 747', 350, '2022-04-20', 1);
+-- Insertion dans la table Contrat
+INSERT INTO Contrat (numC, dateCon, dateFin, etatCon,)
+VALUES (1, '25/12/2020', '30/06/2021','correct'),
+       (2, '14/06/2021', '20/06/2022','correct'),
+       (3, '10/01/2023', '05/05/2024','bien'),
 
--- Insertion dans la table Pilote
-INSERT INTO Pilote (idpilote, nom, prenom, adresse, email, nbvols)
-VALUES (1, 'Dupont', 'Jean', '123 rue des Lilas, Paris', 'jean.dupont@email.com', 50),
-       (2, 'Martin', 'Marie', '456 avenue des Roses, Lyon', 'marie.martin@email.com', 30),
-       (3, 'Lefevre', 'Pierre', '789 chemin des Champs, Marseille', 'pierre.lefevre@email.com', 40);
+-- Insertion dans la table Proprietaire
+INSERT INTO Proprietaire (idproprietaire, dateDebCollab, dateFinCollab)
+VALUES (1, '30/12/2020','05/01/2020'),
+       (2, '20/06/2021','30/06/2021'),
+       (3, '15/01/2023','25/01/2023'),
 
--- Insertion dans la table Vol
-INSERT INTO Vol (idvol, datevol, designation, idavion, idPilote1, idpilote2)
-VALUES (1, '2022-07-05', 'Paris - New York', 1, 1, 2),
-       (2, '2022-08-10', 'Lyon - Berlin', 2, 2, 3),
-       (3, '2022-09-15', 'Marseille - Tokyo', 3, 3, 1);
+-- Insertion dans la table Equipement
+INSERT INTO Equipement (codeAC, nomAC, prixAC,)
+VALUES (1, 'Ski', '40',),
+       (2, 'snowboard', '60'),
+       (3, 'spa','25');
 
+-- Insertion dans la table Stations
+INSERT INTO Stations (codeSta, nomSta, adresse, CP, ville)
+VALUES (1, 'bura', '25 avenue saint martin', '73150', 'Savoie'),
+       (2, 'monou', '30 rue des plantes', '02350', 'haut-alpes'),
+       (, 'ponthiere', '60 avenue boustifiere', '35096', 'rennes'),
+
+
+-- Insertion dans la table Region
+INSERT INTO Region (codeREG, nomReg,)
+VALUES (1, 'Savoie'),
+       (2, 'Haut-alpes'),
+       (3, 'Rennes');
 --
 -- Déchargement des données de la table `utilisateur`
 --
